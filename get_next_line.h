8125c66e72f4aeaa94ce0c8cc9bf6eb8344a3485
Xavier Anglada Gros <xanglada@car14s3.42barcelona.com>
2025-06-17 14:48:00 +0200

    17/6 2

--- get_next_line.h
+++ get_next_line.h
@@ -1,2 +1,10 @@
 
-char *get_next_line(int fd);
+char 	*get_next_line(int fd);
+size_t  ft_strlen(const char *str);
+char    *ft_strrchr(const char *str, int c);
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,66 @@
 /*   By: xanglada <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 12:12:47 by xanglada          #+#    #+#             */
-/*   Updated: 2025/06/17 12:12:59 by xanglada         ###   ########.fr       */
+/*   Updated: 2025/06/17 14:46:35 by xanglada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		str++;
+	}
+	if ((char)c == '\0')
+		return ((char *)str);
+	return (NULL);
+}
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+void	free_all(char **ret_str)
+{
+	int	i;
+
+	i = 0;
+	while (ret_str[i])
+	{
+		free(ret_str[i]);
+		i++;
+	}
+	free(ret_str);
+}
